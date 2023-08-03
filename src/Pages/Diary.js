eea3d5c5ca2db7b7bc1e150d45b8707338541228
yangpahh <126546165+yangpahh@users.noreply.github.com>
2023-08-03 19:04:13 +0900

    230714

---
+++ src/Pages/Diary.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import useDiary from '../hooks/useDiary';
+import Header from '../Components/Header';
+import Button from '../Components/Button';
+import { getFormattedDate } from '../util';
+import Viewer from '../Components/Viewer';
+
+const Diary = () => {
+  const { id } = useParams();
+  const data = useDiary(id);
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+  if (!data) {
+    return <div>일기를 불러오고 있습니다...</div>;
+  } else {
+    const { date, emotionId, content } = data;
+    const title = `${getFormattedDate(new Date(Number(date)))} 기록`;
+    return (
+      <div>
+        <Header
+          title={title}
+          leftChild={<Button text={'< 뒤로 가기'} onClick={goBack} />}
+          rightChild={<Button text={'수정하기'} onClick={goEdit} />}
+        />
+        <Viewer content={content} emotionId={emotionId} />
+      </div>
+    );
+  }
+};
+
+export default Diary;
---
+++ src/Pages/New.js
@@ -0,0 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '../Components/Button';
+import Header from '../Components/Header';
+import Editor from '../Components/Editor';
+import { DiaryDispatchContext } from '../App';
+import React, { useContext } from 'react';
+
+const New = () => {
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
+  const onSubmit = (data) => {
+    const { date, content, emotionId } = data;
+    onCreate(date, content, emotionId);
+    navigate('/', { replace: true });
+  };
+  return (
+    <div>
+      <Header
+        title={'새 일기 쓰기'}
+        leftChild={<Button text={'< 뒤로 가기'} onClick={goBack} />}
+      />
+      <Editor onSubmit={onSubmit} />
+    </div>
+  );
+};
+
+export default New;
